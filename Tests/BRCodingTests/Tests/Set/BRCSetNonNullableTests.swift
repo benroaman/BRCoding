a3a4971d12100aab0a081e5df7479150c9f2642a
Ben Roaman <benroaman@Bens-MacBook-Pro-3.local>
2025-03-14 11:29:00 -0400

    refactored set tests

---
+++ Tests/BRCodingTests/Tests/Set/BRCSetNonNullableTests.swift
@@ -0,0 +1,76 @@
+//
+//  BRCSetNonNullableTests.swift
+//
+//
+//  Created by Ben Roaman on 3/14/25.
+//
+
+import XCTest
+@testable import BRCoding
+
+final class BRCSetNonNullableTests: XCTestCase {
+    // MARK: Coders
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    
+    // MARK: NonNullable
+    private struct SetNonNullable: Codable {
+        @BRCSetNonNullable private(set) var testValue: Set<Int>
+    }
+    
+    // MARK: Decoding Tests
+    func testValidSetDecodesCorrectly() {
+        if let setTest = try? decoder.decode(SetNonNullable.self, from: SetTestData.setJSONData) {
+            XCTAssert(setTest.testValue == SetTestData.set, "Set decoded incorrectly")
+        } else {
+            XCTFail("Set failed to decode")
+        }
+    }
+    
+    func testDecodingArrayWithRedundanciesProducesCorrectSet() {
+        if let test = try? decoder.decode(SetNonNullable.self, from: SetTestData.arrayJSONData) {
+            XCTAssert(test.testValue == Set(SetTestData.array), "Array decoded incorrectly")
+        } else {
+            XCTFail("Array failed to decode")
+        }
+    }
+    
+    func testInvalidTypeDecodesToEmptySet() {
+        if let test = try? decoder.decode(SetNonNullable.self, from: SetTestData.invalidTypeJSONData) {
+            XCTAssert(test.testValue == [], "Invalid Type decoded incorrectly")
+        } else {
+            XCTFail("Invalid Type failed to decode")
+        }
+    }
+    
+    func testNullLiteralDecodesToEmptySet() {
+        if let test = try? decoder.decode(SetNonNullable.self, from: GeneralTestData.nullLiteralJSONData) {
+            XCTAssert(test.testValue == [], "Null Literal decoded incorrectly")
+        } else {
+            XCTFail("Null Literal failed to decode")
+        }
+    }
+    
+    func testMissingFieldDecodesToEmptySet() {
+        if let test = try? decoder.decode(SetNonNullable.self, from: GeneralTestData.missingFieldJSONData) {
+            XCTAssert(test.testValue == [], "Missing field decoded incorrectly")
+        } else {
+            XCTFail("Missing field failed to decode")
+        }
+    }
+    
+    // MARK: Encoding Tests
+    func testEncodingSet() {
+        let test = SetNonNullable(testValue: SetTestData.set)
+        
+        if let encoded = try? encoder.encode(test) {
+            if let decoded = try? decoder.decode(SetNonNullable.self, from: encoded) {
+                XCTAssert(decoded.testValue == SetTestData.set, "Set encoded then decoded incorrectly")
+            } else {
+                XCTFail("Set encoded then failed to decode")
+            }
+        } else {
+            XCTFail("Set failed to encode")
+        }
+    }
+}
