171bd7462a5dfe4ab1336d5fd31d32e2f1c1d760
Ben Roaman <benroaman@Bens-MacBook-Pro-3.local>
2025-03-14 11:23:38 -0400

    renaming and some light refactoring in enum tests

--- Tests/BRCodingTests/Tests/Enum/BRCEnumNonNullableTests.swift
+++ Tests/BRCodingTests/Tests/Enum/BRCEnumNonNullableTests.swift
@@ -1,6 +1,6 @@
 //
-//  BRCEnumTests.swift
-//  
+//  BRCEnumNonNullableTests.swift
+//
 //
 //  Created by Ben Roaman on 3/14/25.
 //
@@ -8,7 +8,7 @@
 import XCTest
 @testable import BRCoding
 
-final class BRCEnumTests: XCTestCase {
+final class BRCEnumNonNullableTests: XCTestCase {
     // MARK: Coders
     private let decoder = JSONDecoder()
     private let encoder = JSONEncoder()
@@ -18,47 +18,24 @@ final class BRCEnumTests: XCTestCase {
         @BRCEnumNonNullable private(set) var testValue: EnumTestData.TestEnum
     }
 
-    func testEnumNonNullableWithValidCase() {
+    // MARK: Decoding Tests
+    func testValidCaseDecodesCorrectly() {
         if let validCaseTest = try? decoder.decode(EnumNonNullable.self, from: EnumTestData.validCaseJSONData) {
             XCTAssert(validCaseTest.testValue == EnumTestData.validCase, "Valid case decoded incorrectly")
-            
-            if let encoded = try? encoder.encode(validCaseTest) {
-                XCTAssert(String(data: encoded, encoding: .utf8) == EnumTestData.validCaseJSON, "Valid case encoding produced bad JSON")
-                
-                if let decoded = try? decoder.decode(EnumNonNullable.self, from: encoded) {
-                    XCTAssert(decoded.testValue == EnumTestData.validCase, "Valid case encoded then decoded incorrectly")
-                } else {
-                    XCTFail("Valid case encoded failed to decode")
-                }
-            } else {
-                XCTFail("Valid case failed to encode")
-            }
         } else {
             XCTFail("Valid case failed to decode")
         }
     }
-        
-    func testEnumNonNullableWithInvalidCase() {
+    
+    func testInvalidCaseDecodesToFallbackCase() {
         if let invalidCaseTest = try? decoder.decode(EnumNonNullable.self, from: EnumTestData.invalidCaseJSONJData) {
             XCTAssert(invalidCaseTest.testValue == EnumTestData.TestEnum.fallbackCase, "Invalid case decoded incorrectly")
-            
-            if let encoded = try? encoder.encode(invalidCaseTest) {
-                XCTAssert(String(data: encoded, encoding: .utf8) == EnumTestData.fallbackJSON, "Invalid case encoding produced bad JSON")
-                
-                if let decoded = try? decoder.decode(EnumNonNullable.self, from: encoded) {
-                    XCTAssert(decoded.testValue == EnumTestData.TestEnum.fallbackCase, "Invalid case encoded then decoded incorrectly")
-                } else {
-                    XCTFail("Invalid case encoded failed to decode")
-                }
-            } else {
-                XCTFail("Invalid case failed to encode")
-            }
         } else {
             XCTFail("Invalid case failed to decode")
         }
     }
     
-    func testEnumNonNullableWithInvalidType() {
+    func testInvalidTypeDecodesToFallbackCase() {
         if let invalidTypeTest = try? decoder.decode(EnumNonNullable.self, from: EnumTestData.invalidTypeJSONData) {
             XCTAssert(invalidTypeTest.testValue == EnumTestData.TestEnum.fallbackCase, "Invalid type decoded incorrectly")
         } else {
@@ -66,7 +43,7 @@ final class BRCEnumTests: XCTestCase {
         }
     }
     
-    func testEnumNonNullableWithNullLiteral() {
+    func testNullLiteralDecodesToFallbackCase() {
         if let nullLiteralTest = try? decoder.decode(EnumNonNullable.self, from: GeneralTestData.nullLiteralJSONData) {
             XCTAssert(nullLiteralTest.testValue == EnumTestData.TestEnum.fallbackCase, "Null literal decoded incorrectly")
         } else {
@@ -74,11 +51,29 @@ final class BRCEnumTests: XCTestCase {
         }
     }
     
-    func testEnumNonNullableWithMissingField() {
+    func testMissingFieldDecodesToFallbackCase() {
         if let missingFieldTest = try? decoder.decode(EnumNonNullable.self, from: GeneralTestData.missingFieldJSONData) {
             XCTAssert(missingFieldTest.testValue == EnumTestData.TestEnum.fallbackCase, "Missing field decoded incorrectly")
         } else {
             XCTFail("Missing field failed to decode")
+        }
+    }
+    
+    // MARK: Encoding Tests
+    func testEncoding() {
+        func testValidCaseDecodesCorrectly() {
+            let test = EnumNonNullable(testValue: EnumTestData.validCase)
+            if let encoded = try? encoder.encode(test) {
+                XCTAssert(String(data: encoded, encoding: .utf8) == EnumTestData.validCaseJSON, "Valid case encoding produced bad JSON")
+                
+                if let decoded = try? decoder.decode(EnumNonNullable.self, from: encoded) {
+                    XCTAssert(decoded.testValue == EnumTestData.validCase, "Valid case encoded then decoded incorrectly")
+                } else {
+                    XCTFail("Valid case encoded then failed to decode")
+                }
+            } else {
+                XCTFail("Valid case failed to encode")
+            }
         }
     }
 }
